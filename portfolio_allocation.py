81cc4599e05fb335a83441834c791a67de115d17
LinnJuge <linnjugesundell@gmail.com>
2025-02-14 19:13:43 +0100

    Update portfolio_allocation.py

--- portfolio_allocation.py
+++ portfolio_allocation.py
@@ -2,17 +2,13 @@ import numpy as np
 from scipy.optimize import minimize
 
 def equal_weighted_portfolio(returns):
-    """
-    Calcule un portefeuille équipondéré.
-    """
-    n = len(returns.columns)
+    """Crée un portefeuille équipondéré."""
+    n = returns.shape[1]
     return np.ones(n) / n  
 
 def min_variance_portfolio(returns):
-    """
-    Optimisation d'un portefeuille à variance minimale.
-    """
-    n = len(returns.columns)
+    """Optimisation d'un portefeuille à variance minimale."""
+    n = returns.shape[1]
     initial_guess = np.ones(n) / n  
     cov_matrix = returns.cov()
 
@@ -23,4 +19,4 @@ def min_variance_portfolio(returns):
     bounds = tuple((0.05, 0.95) for _ in range(n))
 
     result = minimize(portfolio_volatility, initial_guess, constraints=constraints, bounds=bounds)
-    return result.x if result.success else initial_guess 
+    return result.x if result.success else initial_guess
